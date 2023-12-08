8b3e1ad325c86cf938c341eecd0e8eba4b3f5792
unknown <imranzulkarnaen46@gmail.com>
2023-12-08 22:21:12 +0700

    Player

---
+++ MP3Player/src/Model/Lirik.java
@@ -0,0 +1,25 @@
+
+package Model;
+
+public class Lirik {
+    private String lirik;
+
+    public Lirik(String lirik) {
+        this.lirik = lirik;
+    }
+
+    public String getLirik() {
+        return lirik;
+    }
+
+    public void setLirik(String lirik) {
+        this.lirik = lirik;
+    }
+    
+    public void tambahLirik(){
+        
+    }
+    public void tambahLirikOtomatis(){
+        
+    }
+}
